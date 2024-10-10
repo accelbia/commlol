16ceb81fad7b592e842c2bbde5a6a2611850bc07
Ayush Barik <67522615+accelbia@users.noreply.github.com>
2024-10-10 10:09:59 +0000

    lolPage

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div class="gradient-background"></div>
+
+      <div class="center">
+          <img src="https://nextgen.group/hs-fs/hubfs/Commvault-white-logo-new.png?width=2000&height=385&name=Commvault-white-logo-new.png" width="200" height="40" alt='commvault logo'/>
+          Arun is bored of his work in Commvault. Give him better projects. Otherwise he will steal all your data!
+      </div>
+    </>
+  );
+}
+
+export default App;
